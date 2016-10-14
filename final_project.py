3a7715f4d700ec6cbd0541fbbc73ca369dc4e9ea
jiajia <angelajiasun@gmail.com>
2016-10-14 16:14:28 -0400

    new commitment

---
+++ final_project.py
@@ -0,0 +1,30 @@
+#imports
+import os
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+import giphypop
+
+g = giphypop.Giphy()
+
+#functions for results and about page
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/results')
+def results():
+	search_terms = request.values.get('search_terms')
+	if search_terms == "":
+		return render_template('results.html')
+	else:
+		gif_keywords = ' '.join(search_terms.split())
+		gif_results = g.search(gif_keywords)
+		return render_template('results.html', gif_results=gif_results, gif_keywords=gif_keywords)
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
